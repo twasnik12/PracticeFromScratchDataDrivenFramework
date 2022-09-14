d658ea83d618805d6ac0ca6b7b3771b98d6b0c16
twasnik12 <tathagat.wasnik@cogniwize.com>
2022-09-14 12:01:54 +0530

    changes

---
+++ src/main/java/com/PS/utilities/ExtendsReportGenarator.java
@@ -0,0 +1,40 @@
+package com.PS.utilities;
+
+
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+
+public class ExtendsReportGenarator {
+
+	public static ExtentReports extent;
+
+	public static ExtentReports extentreprts() {
+
+		String str = "C:\\Users\\Tathagat\\eclipse-workspace\\PracticeFromScratch\\Reports";
+
+		ExtentSparkReporter reporter = new ExtentSparkReporter(str);
+
+		reporter.config().setDocumentTitle("IRCTC Automation Project");
+
+		reporter.config().setReportName("Captcha Automation");
+
+		reporter.config().setTheme(Theme.DARK);
+
+		ExtentReports extent = new ExtentReports();
+
+		extent.attachReporter(reporter);
+
+		extent.setSystemInfo("Project Name", "Captcha Automation");
+
+		extent.setSystemInfo("User Name", "Tejaswi Pachpute");
+
+		extent.setSystemInfo("EmpId", "CZ103");
+
+		extent.setSystemInfo("Environment", "Automation Testing");
+
+		extent.setSystemInfo("State Demo", " All State is Passed");
+
+		return extent;
+	}
+}
---
+++ src/main/java/com/PS/utilities/ObjectRepo.java
@@ -0,0 +1,10 @@
+package com.PS.utilities;
+
+
+import com.aventstack.extentreports.ExtentTest;
+
+
+public class ObjectRepo {
+	public static ExtentTest test;
+
+}
